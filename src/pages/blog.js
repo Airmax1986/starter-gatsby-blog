92417aade751dceef80b6b4e7ca20c1ea1476adb
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-17 13:50:48 +0200

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -29,7 +29,7 @@ export const pageQuery = graphql`
       nodes {
         title
         slug
-        publishDate(formatString: "MMMM Do, YYYY")
+        Date(formatString: "MMMM Do, YYYY")
         tags
         heroImage {
           gatsbyImage(
