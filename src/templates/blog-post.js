3a3d4e1f7c61ae18eab9f594e40963fb3753fd99
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-17 12:40:06 +0200

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,124 +1,48 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
-import get from 'lodash/get'
-import { renderRichText } from 'gatsby-source-contentful/rich-text'
-import { documentToPlainTextString } from '@contentful/rich-text-plain-text-renderer'
-import { BLOCKS } from '@contentful/rich-text-types'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import readingTime from 'reading-time'
+import React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
 
-import Seo from '../components/seo'
-import Layout from '../components/layout'
-import Hero from '../components/hero'
-// import Tags from '../components/tags' // optional
-import * as styles from './blog-post.module.css'
+const BlogPostTemplate = ({ data }) => {
+  const post = data.contentfulBlogPost
+  const image = getImage(post.headerImage)
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = get(this.props, 'data.contentfulBlogPost')
-    const previous = get(this.props, 'data.previous')
-    const next = get(this.props, 'data.next')
-    const plainTextSummary = post.summary
-    const plainTextBody = post.content
+  return (
+    <Layout>
+      <article>
+        <h1>{post.title}</h1>
+        <p><em>{post.date}</em> &nbsp;–&nbsp; von {post.author?.name}</p>
 
-    const { minutes: timeToRead } = readingTime(plainTextBody)
+        {image && (
+          <GatsbyImage image={image} alt={post.title} style={{ marginBottom: "2rem" }} />
+        )}
 
-    const options = {
-      renderNode: {
-        [BLOCKS.EMBEDDED_ASSET]: (node) => {
-          const { gatsbyImage, description } = node.data.target
-          return (
-            <GatsbyImage
-              image={getImage(gatsbyImage)}
-              alt={description}
-            />
-          )
-        },
-      },
-    }
+        <p>{post.summary}</p>
 
-    return (
-      <Layout location={this.props.location}>
-        <Seo
-          title={post.title}
-          description={plainTextSummary}
-          image={`http:${post.headerImage?.resize?.src}`}
-        />
-        <Hero
-          image={post.headerImage?.gatsbyImage}
-          title={post.title}
-          content={post.summary}
-        />
-        <div className={styles.container}>
-          <span className={styles.meta}>
-            {post.author?.name} &middot;{' '}
-            <time dateTime={post.date}>{post.date}</time> –{' '}
-            {timeToRead} minute read
-          </span>
-          <div className={styles.article}>
-            <div className={styles.body}>
-              {post.content && <p>{post.content}</p>}
-            </div>
-            {/* Tags entfernen oder selbst einbauen */}
-            {/* <Tags tags={post.tags} /> */}
-            {(previous || next) && (
-              <nav>
-                <ul className={styles.articleNavigation}>
-                  {previous && (
-                    <li>
-                      <Link to={`/blog/${previous.slug}`} rel="prev">
-                        ← {previous.title}
-                      </Link>
-                    </li>
-                  )}
-                  {next && (
-                    <li>
-                      <Link to={`/blog/${next.slug}`} rel="next">
-                        {next.title} →
-                      </Link>
-                    </li>
-                  )}
-                </ul>
-              </nav>
-            )}
-          </div>
+        <div style={{ marginTop: "2rem", lineHeight: "1.6" }}>
+          {post.content}
         </div>
-      </Layout>
-    )
-  }
+      </article>
+    </Layout>
+  )
 }
 
 export default BlogPostTemplate
 
 export const pageQuery = graphql`
-  query BlogPostBySlug(
-    $slug: String!
-    $previousPostSlug: String
-    $nextPostSlug: String
-  ) {
+  query BlogPostBySlug($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
-      slug
       title
+      slug
       summary
-      date(formatString: "MMMM Do, YYYY")
-      headerImage {
-        gatsbyImage(layout: FULL_WIDTH, placeholder: BLURRED, width: 1280)
-        resize(height: 630, width: 1200) {
-          src
-        }
-      }
       content
+      date(formatString: "MMMM D, YYYY")
+      headerImage {
+        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, width: 1000)
+      }
       author {
         name
       }
-    }
-    previous: contentfulBlogPost(slug: { eq: $previousPostSlug }) {
-      slug
-      title
-    }
-    next: contentfulBlogPost(slug: { eq: $nextPostSlug }) {
-      slug
-      title
     }
   }
 `
