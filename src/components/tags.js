491ef8abd03d98c4a5f9bc0e13f49b78d429eab9
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-17 14:56:48 +0200

    Update tags.js

--- src/components/tags.js
+++ src/components/tags.js
@@ -2,19 +2,18 @@ import React from 'react'
 
 import * as styles from './tags.module.css'
 
-const Tags = ({ tags }) =>
-  tags?.length > 0 && (
-    <small className={styles.tags}>
-      {tags && tags.length > 0 && tags.split(',').map((tag) => (
-  <div key={tag.trim()} className={styles.tag}>
-    {tag.trim()}
+const Tags = ({ tags }) => (
+  <div className={styles.container}>
+    {tags && tags.length > 0 && (
+      <small className={styles.tags}>
+        {tags.split(',').map((tag) => (
+          <div key={tag.trim()} className={styles.tag}>
+            {tag.trim()}
+          </div>
+        ))}
+      </small>
+    )}
   </div>
-))}
-        <div key={tag} className={styles.tag}>
-          {tag}
-        </div>
-      ))}
-    </small>
-  )
+)
 
 export default Tags
