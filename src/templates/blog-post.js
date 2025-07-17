6ac6d1ded4590895db80eb0cc40beebb1e97347a
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-17 16:54:36 +0200

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,57 +1,25 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import Layout from "../components/layout"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { remark } from "remark"
+import html from "remark-html"
+import { useEffect, useState } from "react"
 
 const BlogPostTemplate = ({ data }) => {
   const post = data.contentfulBlogPost
-  const image = getImage(post.headerImage)
+  const [htmlContent, setHtmlContent] = useState("")
+
+  useEffect(() => {
+    async function convertMarkdown() {
+      const file = await remark().use(html).process(post.content)
+      setHtmlContent(file.toString())
+    }
+    convertMarkdown()
+  }, [post.content])
 
   return (
     <Layout>
-      <article style={{ maxWidth: "720px", margin: "0 auto", padding: "2rem" }}>
-        <h1>{post.title}</h1>
-        <p style={{ color: "#777", fontStyle: "italic" }}>
-          {post.Date} &nbsp;–&nbsp; von {post.author?.name}
-        </p>
-
-        {image && (
-          <GatsbyImage
-            image={image}
-            alt={post.title}
-            style={{ margin: "2rem 0", borderRadius: "8px" }}
-          />
-        )}
-
-        <p>{post.summary}</p>
-
-        <section style={{ marginTop: "2rem", lineHeight: "1.7" }}>
-          {documentToReactComponents(post.content.json)}
-        </section>
+      <article>
+        {/* ... */}
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
       </article>
     </Layout>
   )
 }
-
-export default BlogPostTemplate
-
-export const pageQuery = graphql`
-  query BlogPostBySlug($slug: String!) {
-    contentfulBlogPost(slug: { eq: $slug }) {
-      title
-      slug
-      summary
-      content {
-        json
-      }
-      Date(formatString: "MMMM D, YYYY")
-      headerImage {
-        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, width: 1000)
-      }
-      author {
-        name
-      }
-    }
-  }
-`
