c7a5b0340d69de1364f6dc80c1144782a30af3f9
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-17 17:15:09 +0200

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,27 +1,53 @@
-import { remark } from "remark"
-import html from "remark-html"
-import { useEffect, useState } from "react"
+import React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import Layout from "../components/layout"
 
 const BlogPostTemplate = ({ data }) => {
   const post = data.contentfulBlogPost
-  const [htmlContent, setHtmlContent] = useState("")
-
-  useEffect(() => {
-    async function convertMarkdown() {
-      const file = await remark().use(html).process(post.content)
-      setHtmlContent(file.toString())
-    }
-    convertMarkdown()
-  }, [post.content])
+  const image = getImage(post.headerImage)
 
   return (
     <Layout>
       <article>
-        {/* ... */}
-        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+        <h1>{post.title}</h1>
+        <p><em>{post.Date}</em> &nbsp;–&nbsp; von {post.author?.name}</p>
+        {image && (
+          <GatsbyImage image={image} alt={post.title} style={{ marginBottom: "2rem", borderRadius: "8px" }} />
+        )}
+        <p>{post.summary}</p>
+
+        {/* Hier wird der Markdown-Content als HTML ausgegeben */}
+        <div
+          className="article-content"
+          dangerouslySetInnerHTML={{ __html: post.content.childMarkdownRemark.html }}
+        />
       </article>
     </Layout>
   )
 }
 
 export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      slug
+      summary
+      Date(formatString: "MMMM D, YYYY")
+      headerImage {
+        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, width: 1000)
+      }
+      author {
+        name
+      }
+      content {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`
+
