be2712346cf704c609f1c82bfd73a8ae0b35eb40
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-17 15:27:01 +0200

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import get from 'lodash/get'
-
 import Seo from '../components/seo'
 import Layout from '../components/layout'
 import Hero from '../components/hero'
@@ -10,12 +9,20 @@ import ArticlePreview from '../components/article-preview'
 class BlogIndex extends React.Component {
   render() {
     const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
-
+    
     return (
       <Layout location={this.props.location}>
         <Seo title="Blog" />
         <Hero title="Blog" />
-        <ArticlePreview posts={posts} />
+        <div className="wrapper">
+          <ul className="article-list">
+            {posts.map(post => (
+              <li key={post.slug}>
+                <ArticlePreview post={post} />
+              </li>
+            ))}
+          </ul>
+        </div>
       </Layout>
     )
   }
