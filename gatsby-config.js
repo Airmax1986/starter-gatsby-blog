ae97857bb68fefbc97687923eece3f97694b33bf
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-17 13:13:17 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-react-helmet", 
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
     {
@@ -17,27 +17,8 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        // Host entfernen oder explizit setzen
-        host: process.env.CONTENTFUL_HOST || "cdn.contentful.com",
-        // Environment explizit setzen
         environment: "master",
       },
     },
-    // Markdown-Transformer für Contentful hinzufügen
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-images-contentful",
-            options: {
-              maxWidth: 590,
-            },
-          },
-        ],
-      },
-    },
-    // Netlify Plugin am Ende hinzufügen
-    "gatsby-plugin-netlify",
   ],
 };
