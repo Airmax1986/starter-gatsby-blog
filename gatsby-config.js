96dc9f61fe19b0cfc86627a2ad047f9732878acf
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-17 16:51:05 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "BrandKernel.io",
+    description: "Craft your brand kernel with AI flows.",
   },
   plugins: [
     "gatsby-transformer-sharp",
